5b2a51158e15e853d458b09caa04d60c6624bb20
Wongsakon Hongsala <85827477+Coke3a@users.noreply.github.com>
2025-11-25 21:57:14 +0700

    Implement recording and subscription queries

--- src/infrastructure/postgres/repositories/subscriptions.rs
+++ src/infrastructure/postgres/repositories/subscriptions.rs
@@ -1,5 +1,7 @@
 use anyhow::Result;
 use axum::async_trait;
+use chrono::Utc;
+use diesel::{RunQueryDsl, insert_into, prelude::*, update};
 use std::sync::Arc;
 use uuid::Uuid;
 
@@ -7,8 +9,12 @@ use crate::{
     domain::{
         entities::{plans::PlanEntity, subscriptions::InsertSubscriptionEntity},
         repositories::subscriptions::SubscriptionRepository,
+        value_objects::enums::subscription_statuses::SubscriptionStatus,
     },
-    infrastructure::postgres::postgres_connection::PgPoolSquad,
+    infrastructure::postgres::{
+        postgres_connection::PgPoolSquad,
+        schema::{plans, subscriptions},
+    },
 };
 
 pub struct SubscriptionPostgres {
@@ -24,18 +30,52 @@ impl SubscriptionPostgres {
 #[async_trait]
 impl SubscriptionRepository for SubscriptionPostgres {
     async fn list_plans(&self) -> Result<Vec<PlanEntity>> {
-        unimplemented!()
+        let mut conn = Arc::clone(&self.db_pool).get()?;
+
+        let results = plans::table
+            .filter(plans::is_active.eq(true))
+            .select(PlanEntity::as_select())
+            .load::<PlanEntity>(&mut conn)?;
+
+        Ok(results)
     }
     async fn subscribe(
         &self,
         insert_subscription_entity: InsertSubscriptionEntity,
     ) -> Result<Uuid> {
-        unimplemented!()
+        let mut conn = Arc::clone(&self.db_pool).get()?;
+
+        let result = insert_into(subscriptions::table)
+            .values(&insert_subscription_entity)
+            .returning(subscriptions::id)
+            .get_result::<Uuid>(&mut conn)?;
+
+        Ok(result)
     }
     async fn cancel_subscription(&self, subscription_id: Uuid) -> Result<()> {
-        unimplemented!()
+        let mut conn = Arc::clone(&self.db_pool).get()?;
+
+        update(subscriptions::table)
+            .filter(subscriptions::id.eq(subscription_id))
+            .set((
+                subscriptions::status.eq(SubscriptionStatus::Canceled.to_string()),
+                subscriptions::canceled_at.eq(Some(Utc::now())),
+            ))
+            .execute(&mut conn)?;
+
+        Ok(())
     }
     async fn check_current_user_subscription(&self, user_id: Uuid) -> Result<bool> {
-        unimplemented!()
+        let mut conn = Arc::clone(&self.db_pool).get()?;
+
+        let current_subscription = subscriptions::table
+            .filter(subscriptions::user_id.eq(user_id))
+            .filter(subscriptions::status.eq(SubscriptionStatus::Active.to_string()))
+            .filter(subscriptions::ends_at.gt(Utc::now()))
+            .select(subscriptions::id)
+            .first::<Uuid>(&mut conn)
+            .optional()?;
+
+        Ok(current_subscription.is_some())
     }
 }
