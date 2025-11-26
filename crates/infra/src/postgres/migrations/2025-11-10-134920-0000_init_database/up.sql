763eb89d9f6807d4b720873050f622becc1cac29
Coke2a <kotori373510@gmail.com>
2025-11-26 22:18:07 +0700

    update sql

--- crates/infra/src/postgres/migrations/2025-11-10-134920-0000_init_database/up.sql
+++ crates/infra/src/postgres/migrations/2025-11-10-134920-0000_init_database/up.sql
@@ -317,3 +317,23 @@ ALTER TABLE "deliveries"
 
 ALTER TABLE "deliveries"
   ADD FOREIGN KEY ("user_id") REFERENCES "app_users"("id");
+
+  
+-- trigger when create user in subapase
+create or replace function public.handle_new_user()
+returns trigger
+language plpgsql
+security definer set search_path = public
+as $$
+begin
+insert into public.app_users (id, display_name, status)
+values (new.id, new.raw_user_meta_data->>'display_name', 'active');
+return new;
+end;
+$$;
+
+drop trigger if exists on_auth_user_created on auth.users;
+
+create trigger on_auth_user_created
+after insert on auth.users
+for each row execute procedure public.handle_new_user();
