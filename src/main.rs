63eb2e55f688a2044ef17dd7af802ae542cbb249
Coke2a <kotori373510@gmail.com>
2025-11-28 14:29:24 +0700

    delete unuse file

--- src/main.rs
+++
@@ -1,9 +0,0 @@
-use tracing::error;
-
-#[tokio::main]
-async fn main() {
-    if let Err(error) = backend::run().await {
-        error!("Backend exited with error: {}", error);
-        std::process::exit(1);
-    }
-}
