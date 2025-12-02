d459a00a9e8cedc0404a207873e8fe2e9006a1df
Coke2a <kotori373510@gmail.com>
2025-12-02 17:27:04 +0700

    refactor: relocate tracing subscriber initialization from main to lib.rs

--- worker/src/main.rs
+++ worker/src/main.rs
@@ -4,8 +4,6 @@ use tracing_subscriber::fmt;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    fmt().with_max_level(tracing::Level::DEBUG).init();
-
     if let Err(error) = worker::run().await {
         error!("Worker exited with error: {}", error);
         std::process::exit(1);
